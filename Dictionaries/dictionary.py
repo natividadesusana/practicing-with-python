8e16d210341671665215e8b8615cd157f31b059e
natividadesusana <susanajdsn@gmail.com>
2022-02-14 19:40:23 -0300

    first commit

---
+++ Dictionaries/dictionary.py
@@ -0,0 +1,11 @@
+users = {}
+print(users)
+
+users = {
+    'chaves' : { 'Chaves do 8', '14/02/2022', 'Recep_01'},
+    'quico' : {'Quico das Flores', '10/02/2022', 'Raiox_03'}
+}
+print(users)
+
+users['florinda'] = ['Dona Florinda', '14/02/2022', 'Raiox_01']
+print(users)
